7e932074aaaf371f02059e675da3ca5eac06dc1c
Ben Velie <67563877+veliebm@users.noreply.github.com>
2021-06-11 11:47:37 -0400

    Create first code skeleton

--- code/run_all.py
+++ code/run_all.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 """
-The master script. Run this to run the entire analysis, start to finish.
+The master script. Run this to run the entire analysis from beginning to end.
 
 Created on 6/11/2021 by Benjamin Velie.
 veliebm@gmail.com
 """
+
+def main():
+    """
+    Drives our entire analysis. Represents months of hard work.
+    """
+    # Check that we have BrainVision analyzer results.
+
+    # Convert our dataset to BIDS.
+    
+    # Check that we have fMRIPrep results.
+
+    # …
+
+    pass
+
+if __name__ == "__main__":
+    """
+    If we call this script from the command line, then do everything.
+    """
+    main()
