b7b8d0e53959213c6bcc22fab104aa1863707ce5
veliebm <veliebm@gmail.com>
2021-06-11 15:50:56 -0400

    Hollow out script so it only calls other scripts

--- code/run_all.py
+++ code/run_all.py
@@ -5,23 +5,22 @@ The master script. Run this to run the entire analysis from beginning to end.
 Created on 6/11/2021 by Benjamin Velie.
 veliebm@gmail.com
 """
+# Import external modules.
+from os import PathLike
+import subprocess
 
 def main():
     """
     Drives our entire analysis. Represents months of hard work.
     """
-    # Check that we have BrainVision analyzer results.
+    run_node("check_raw.py")
 
-    # Convert our dataset to BIDS.
-    
-    # Check that we have fMRIPrep results.
-
-    # …
-
-    pass
+def run_node(path_to_script: PathLike) -> None:
+    """
+    Runs a node in our pipeline.
+    """
+    command = ["python3", path_to_script]
+    subprocess.run(command)
 
 if __name__ == "__main__":
-    """
-    If we call this script from the command line, then do everything.
-    """
     main()
