fc35e00422f86078bfb4b2fb956657e45c12eab7
veliebm <veliebm@gmail.com>
2021-06-24 08:48:40 -0400

    Make compatible with pydoit

--- code/fmriprep.py
+++ code/fmriprep.py
@@ -5,14 +5,17 @@ This node doesn't do much. It simply commands you to run fMRIPrep on the data.
 Created on 6/14/2021 by Benjamin Velie.
 veliebm@gmail.com
 """
+# Import external modules and libraries.
+from os import PathLike
 from pathlib import Path
-def main():
+
+def main(fmriprep_dir: PathLike):
     """
     Command user to run fMRIPrep.
     """
-    outputs_path = Path(f"../outputs/{__name__}")
-    if outputs_path.exists():
-        print(f"Detected {outputs_path}")
+    fmriprep_dir = Path(fmriprep_dir)
+    if fmriprep_dir.exists():
+        print(f"Detected {fmriprep_dir}")
     else:
-        exception_message = f"You must now manually run fMRIPrep on the data. After you run it, place its outputs in {outputs_path}"
+        exception_message = f"You must now manually run fMRIPrep on the data. After you run it, place its outputs in {fmriprep_dir}"
         raise FileNotFoundError(exception_message)
